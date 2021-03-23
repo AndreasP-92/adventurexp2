c15e4b44d591c022fa2c3bcc3329b8c8d7e5f5c8
Rewax <daniel.lorenzen@hotmail.com>
2021-03-23 12:58:55 +0100

    Activity query test

--- back-end-adventurexp/src/main/java/backendadventurexp/demo/controller/ActivityController.java
+++ back-end-adventurexp/src/main/java/backendadventurexp/demo/controller/ActivityController.java
@@ -44,4 +44,5 @@ public class ActivityController {
         return activityRepository.save(activity);
 
     }
+
 }
--- back-end-adventurexp/src/main/java/backendadventurexp/demo/repository/ActivityRepository.java
+++ back-end-adventurexp/src/main/java/backendadventurexp/demo/repository/ActivityRepository.java
@@ -3,8 +3,20 @@ package backendadventurexp.demo.repository;
 
 import backendadventurexp.demo.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     //    List<Activity> findByActivity_name(Long id);
     public Activity findByName(String name);
+
+    @Query("SELECT id, name, description, age, size, practicalinfo, pic, price FROM activity")
+    List<Activity> activityInfo(int id, String name, String description, String age, String size, String practicalinfo, String price)
+
+
+
+    @Query("FROM activity WHERE id,name,description,age,size,practicalinfo,price,pic = ?8")
+    List<Activity> activityInfo(String location);
+
 }
