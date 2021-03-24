56520b09eb597cb4d687763cb0ef184c472eb07b
SoSoCho <70580037+SoSoCho@users.noreply.github.com>
2021-03-24 13:06:07 +0100

    update BE 24/3

---
+++ back-end-adventurexp/src/main/java/backendadventurexp/demo/controller/AdminController.java
@@ -0,0 +1,39 @@
+package backendadventurexp.demo.controller;
+
+
+import backendadventurexp.demo.model.Auth;
+import backendadventurexp.demo.model.Users;
+import backendadventurexp.demo.repository.AuthRepository;
+import backendadventurexp.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthRepository authRepository;
+
+    @PostMapping(value = "/insert/admin/user", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Users insertUser(@RequestBody Users users) {
+        System.out.println(users);
+
+        return userRepository.save(users);
+    }
+
+    @PostMapping(value = "/insert/admin/auth", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Auth insertAuth(@RequestBody Auth auth) {
+        System.out.println(auth);
+
+        return authRepository.save(auth);
+    }
+}
