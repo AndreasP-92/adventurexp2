7a5319c0fc58dfc024bf0a05fe839a874a6a57d0
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2021-03-25 18:12:38 +0100

    update

--- back-end-adventurexp/src/main/java/backendadventurexp/demo/repository/AuthRepository.java
+++ back-end-adventurexp/src/main/java/backendadventurexp/demo/repository/AuthRepository.java
@@ -3,5 +3,8 @@ package backendadventurexp.demo.repository;
 import backendadventurexp.demo.model.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthRepository extends JpaRepository<Auth, Long> {
+    List<Auth> findAllByRole(String mail);
 }
