2e6ba08d40712db29a2e46ba8d641517d55362ea
SoSoCho <70580037+SoSoCho@users.noreply.github.com>
2021-03-26 10:17:16 +0100

    update BE2 26/3

--- back-end-adventurexp/src/main/java/backendadventurexp/demo/controller/BookingController.java
+++ back-end-adventurexp/src/main/java/backendadventurexp/demo/controller/BookingController.java
@@ -71,7 +71,7 @@ public class BookingController {
     @PostMapping(value = "/update/booking", consumes = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public void updateBooking(@RequestBody Booking booking) {
-        System.out.println("BOOKING ==="+booking);
+        System.out.println("BOOKING ===" + booking);
         Booking oneBooking = bookingRepository.findByBookingId(booking.getBookingId());
 //        oneBooking.setBookingClosed(booking.getBookingActive());
         if(oneBooking.getBookingActive() == 0){
@@ -80,8 +80,6 @@ public class BookingController {
             System.out.println("FEJL");
         }
         bookingRepository.save(oneBooking);
+        }
 
     }
-
-
-}
